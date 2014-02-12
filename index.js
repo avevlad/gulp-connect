3a825a440e4179bec73ca05ffdfe4ee85d418dd8
AveVlad <dev@vld.me>
2014-02-12 08:04:46 +0400

    add support array to root

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var lr;
 module.exports = {
   server: function (o) {
     o = o || {};
-    if (!o.root) o.root = 'app';
+    if (!o.root) o.root = ['app'];
     if (!o.port) o.port = 3000;
     if (!o.livereload) o.livereload = false;
     if (o.open) {
@@ -22,35 +22,43 @@ module.exports = {
     if (o.livereload) {
       if (typeof o.livereload == 'boolean') o.livereload = {};
       if (!o.livereload.port) o.livereload.port = 35729;
+      lr = tiny_lr();
+      lr.listen(o.livereload.port);
     }
-    if (!fs.existsSync(o.root)) {
-      util.log(util.colors.red('Folder ' + o.root + ' does not exist!'));
+    var BreakException = function (message) {
+      this.message = message;
+    };
+    try {
+      o.root.forEach(function (path) {
+        if (!fs.existsSync(path)) {
+          throw new BreakException('Folder ' + path + ' does not exist!');
+        }
+      });
+    } catch (e) {
+      util.log(util.colors.red(e.message));
       return false;
     }
     return function () {
       var middleware = o.middleware ? o.middleware.call(this, connect, o) : [];
+      o.root.forEach(function (path) {
+        middleware.push(connect.static(path));
+      });
       if (o.livereload) {
-        lr = tiny_lr();
-        lr.listen(o.livereload.port);
         middleware.push(liveReload({port: o.livereload.port}));
         util.log(util.colors.green('Connect LiveReload on ' + o.livereload.port + ' port'));
       }
-      middleware.push(connect["static"](o.root));
       var app = connect.apply(null, middleware);
       var server = http.createServer(app);
       server
         .listen(o.port)
-        .on('listening', function() {
+        .on('listening', function () {
           var url, browsername;
-
           util.log(util.colors.green('Server started on ' + o.port + ' port'));
           if (o.open) {
             url = 'http://localhost:' + o.port + '/' + o.open.file;
-
             if (o.open.browser) browsername = o.open.browser;
             else browsername = 'default browser';
-
-            open(url, o.open.browser, function(error) {
+            open(url, o.open.browser, function (error) {
               if (error) util.log(util.colors.red(error));
               else util.log(util.colors.green('Opened ' + url + ' in ' + browsername));
             });
