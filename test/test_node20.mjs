f14423961c0651ea07cdb3d525f95458b44f9fdc
avevlad <vlad.derzhavin.mail@gmail.com>
2023-04-24 21:33:05 +0300

    simple test with zero dependency

--- test/test_node20.mjs
+++ test/test_node20.mjs
@@ -1,6 +1,24 @@
-import test from 'node:test';
+import { describe, it } from 'node:test';
 import assert from 'node:assert';
+import request from 'supertest';
+import connect from '../index.js';
 
-test('foo', (t) => {
-  assert.strictEqual(1, 1);
+var portCounter = 35000;
+
+describe('Simple', async () => {
+  it('is a subtest', () => {
+    var port = portCounter++;
+    connect.server({
+      port: port
+    }, function () {
+      request('http://localhost:' + port)
+        .get('/fixtures/simplest/test.txt')
+        .expect(/Hello world/)
+        .expect(200)
+        .end(function (err, res) {
+          done(err);
+        });
+    });
+    assert.ok('some relevant assertion here');
+  });
 });
