3cccc4a710ea5ef77a98460381d3db2c95de33eb
avevlad <vlad.derzhavin.mail@gmail.com>
2023-04-24 21:40:47 +0300

    test: add implicit /index.html

--- test/test_node20.mjs
+++ test/test_node20.mjs
@@ -6,6 +6,7 @@ import connect from '../index.js';
 var portCounter = 35000;
 
 describe('Simple', async () => {
+
   it('is a subtest', () => {
     var port = portCounter++;
     connect.server({
@@ -21,4 +22,19 @@ describe('Simple', async () => {
     });
     assert.ok('some relevant assertion here');
   });
+
+  it('Implicit /index.html', () => {
+    var port = portCounter++;
+    connect.server({
+      port: port
+    }, function () {
+      request('http://localhost:' + port)
+        .get('/fixtures/simplest/')
+        .expect(/index page/)
+        .expect(200)
+        .end(function (err, res) {
+          done(err);
+        });
+    });
+  })
 });
